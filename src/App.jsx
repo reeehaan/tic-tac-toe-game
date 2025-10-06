a5f462d7f5a4e0c64d6e614500b3be823d3ddc89
reeehaan <rehan221104@gmail.com>
2025-10-06 22:23:15 +0530

    feat : add wining condition check

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,95 @@
 import { useState } from "react"
 import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
+import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver";
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+
+//helper function 
+function deriveActivePlayer(gameTurns){
+let currentPlayer = 'X'
+    if (gameTurns.length > 0 && gameTurns[0].player=== 'X'){
+      currentPlayer = 'O';
+    }
+    return currentPlayer;
+}
 
 function App() {
+const [players, setPlayers] = useState({
+  X : 'Player 1',
+  O : 'Player 2'
+});
+const [gameTurns, setGameTurns] = useState([]);
+//const [hasWinner, setHasWinner] = useState(false);
+//const [activePlayer, setActivePlayer] = useState('X');
+const activePlayer = deriveActivePlayer(gameTurns);
+
+let gameBoard = [...initialGameBoard.map(array => [...array])];
+    for(const turn of gameTurns){
+        //destructuring array of objects
+        const {square , player} = turn;
+        //destructing object
+        const {row, col} = square;
+
+        gameBoard[row][col] = player
+    }
+
+let winner; 
+
+for(const combination of WINNING_COMBINATIONS){
+  const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+  const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+  const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
   
-const [activePlayer, setActivePlayer] = useState('X');
-//player after selecting a square
-function handleSelectSquare(){
-  setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X' );
+  if(
+    firstSquareSymbol && 
+    firstSquareSymbol === secondSquareSymbol && 
+    firstSquareSymbol === thirdSquareSymbol
+  ){
+      winner = firstSquareSymbol;
+  }
 }
+
+const hasDraw = gameTurns.length === 9 && !winner;
+//player after selecting a square
+function handleSelectSquare(rowIndex, colIndex){
+  //setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X' );
+  
+  setGameTurns((prevTurns) => {
+    const currentPlayer = deriveActivePlayer(prevTurns);
+
+    const updatedTurns = [
+      {square : 
+        {
+          row : rowIndex ,
+          col : colIndex
+        },
+        player : currentPlayer
+      },...prevTurns];
+
+      return updatedTurns;
+  });
+}
+
+function handleRestart(){
+  setGameTurns([]);
+}
+
+function handlePlayerNameChange(symbol, newName) {
+  setPlayers(prevPlayers => {
+    return{
+      ...prevPlayers,
+      [symbol]: newName
+    };
+  });
+}
+
   return (
    <main>
       <div id="game-container">
@@ -22,12 +103,16 @@ function handleSelectSquare(){
             symbol="O"
             isActive={activePlayer === 'O'}/>
         </ol>
+        {/* game over component */}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
         <GameBoard
           onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
+          board={gameBoard}
         />
       </div>
-      Log
+     <Log
+      turns={gameTurns}
+     />
    </main>
   )
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,34 +1,10 @@
 
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
+export default function GameBoard({onSelectSquare, board}){
 
-export default function GameBoard({onSelectSquare, turns}){
-    let gameBoard = initialGameBoard;
-    for(const turn of turns){
-        //destructuring array of objects
-        const {square , player} = turn;
-        //destructing object
-        const {row, col} = square;
-
-        gameBoard[row][col] = player
-    }
-    // const [gameBoard, setGameBoard]=useState(initialGameBoard);
-
-    // function handleSelectSquare(rowIndex, colIndex){
-    //     setGameBoard((prevGameBoard) => {
-    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //         return updatedBoard;
-    //     });
-    //     onSelectSquare();
-    // }
     return(
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
